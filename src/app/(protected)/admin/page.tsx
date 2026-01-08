0b6202547b51c2012d7e82275eac9f45fce6a262
ppeterson104 <p_thomas_peterson@yahoo.com>
2026-01-08 16:56:44 -0500

    updated admin page

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,5 +1,13 @@
+import { auth } from '@/auth';
 import React from 'react';
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const session=await auth();
+  const role = session?.user.role;
+
+  if (role?.toLowerCase() !== 'admin') {
+return <div>Access Denied</div>
+  } else {
   return <div>AdminPage</div>;
+  }
 }
