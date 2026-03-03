70b35752bd1e160ace3842311b58767a4f376a53
Pete Peterson <pete.peterson@coxautoinc.com>
2026-03-03 10:05:13 -0500

    fixed tw border color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export default async function Header() {
   const userName = session?.user?.name;
   console.log('session', session);
   return (
-    <header className="w-full border-b border-gray-200 bg-white dark:bg-black dark: border-gray-800 sticky top-0 z-50">
+    <header className="w-full border-b border-gray-200 bg-white dark:bg-black dark:border-gray-800 sticky top-0 z-50">
       <div className="max-w-5xl mx-auto px-4 h-14 flex items-center justify-between">
         {/* Left side: Logo or App Name */}
         <div className="flex items-center gap-6">
